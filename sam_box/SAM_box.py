438703ef8e771c6ed8316e5adf2defd3168be710
Pipe <titinunt121@hotmail.com>
2025-02-25 09:12:31 +0800

    update sambox

--- sam_box/SAM_box.py
+++ sam_box/SAM_box.py
@@ -20,6 +20,11 @@ from huggingface_hub import hf_hub_download
 from pycocotools import mask as maskUtils
 from skimage import measure
 import time
+import rasterio
+import base64
+import pickle
+import shapely
+from rasterio.features import geometry_mask
 
 # Function to display the segmentation mask overlay on an axis
 def show_mask(mask, ax, random_color=False):
@@ -47,12 +52,65 @@ def show_box(box, ax):
     w, h = box[2] - box[0], box[3] - box[1]  # Width and height
     ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0, 0, 0, 0), lw=2))
 
+def parse_coord(coord):
+    # Remove any trailing parentheses or unwanted characters
+    coord = coord.strip(")")
+    coord = coord.strip("(")
+    # Split into x and y components and convert to float
+    return tuple(map(float, coord.split()))
+    
+# segmentation multipolygon to box
+def multipolygon_to_box(obj):
+                         
+    coords_str = str(obj).replace("MULTIPOLYGON (((", "").replace(")))", "")
+    # Split into individual coordinate pairs
+    coords_list = coords_str.split(", ")
+
+    # Parse coordinates into a list of (x, y) tuples
+    #coords = [tuple(map(int, coord.split())) for coord in coords_list]
+    #coords = [tuple(map(float, coord.split())) for coord in coords_list]
+    coords = [parse_coord(coord) for coord in coords_list]
+    
+    # Find min and max x and y values
+    min_x = min(coord[0] for coord in coords)
+    max_x = max(coord[0] for coord in coords)
+    min_y = min(coord[1] for coord in coords)
+    max_y = max(coord[1] for coord in coords)
+
+    # Define the bounding box
+    bounding_box = [min_x,min_y,max_x,max_y]
+    return bounding_box
+
 # Converts a polygon to a binary mask
 def polygon_to_mask(polygon, image_height, image_width):
     mask = np.zeros((image_height, image_width), dtype=np.uint8)
     rle = maskUtils.frPyObjects(polygon, image_height, image_width)  # Generate RLE mask
     mask = maskUtils.decode(rle)  # Decode RLE to binary mask
     return mask
+
+def poly_sting_to_multi_polygon(poly_string):
+    pickled_bytes = base64.b64decode(poly_string.encode("utf-8"))
+    obj = pickle.loads(pickled_bytes)
+    return obj
+
+def multi_polygon_to_mask(multi_polygon,height,width):
+    height, width = height, width  
+    transform = rasterio.transform.from_origin(0, height, 1, 1)  
+
+    mask = geometry_mask(
+        [multi_polygon], 
+        transform=transform, 
+        out_shape=(height, width), 
+        invert=True  
+    )
+    return mask
+
+def str_poly_to_mask(polygon, image_height, image_width):
+    multi_poly = poly_sting_to_multi_polygon(polygon)
+    mask = multi_polygon_to_mask(multi_poly,image_height, image_width)
+    mask = np.flip(mask, axis=0) ##dont know why the result from rasterio is not correct so need to flip
+    return mask
+    
 
 # Calculates the Dice coefficient between two masks (a measure of similarity)
 def dice_coefficient(mask1, mask2):
@@ -65,10 +123,13 @@ def dice_coefficient(mask1, mask2):
 
 # Perform segmentation on the image based on ROIs and optional ground truth masks
 def perform_segmentation(predictor, image_path, box_list, expanded_rate=0.15, dice_cal=True, segmentation_poly="", 
-                         label_list = "", visualization=True, show_box=True, show_text=False):
+                         label_list = "", visualization=True, show_box=True, show_text=False, crop =[]):
     #load image
     #print(image_path)
     image = cv2.imread(image_path)
+    
+    if crop!=[]:
+        image = image[crop[1]:crop[3], crop[0]:crop[2]]
     image_source = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     H, W, _ = image_source.shape
     
@@ -81,10 +142,13 @@ def perform_segmentation(predictor, image_path, box_list, expanded_rate=0.15, di
     gt_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     
     #crop roi
+    
     roi_list = box_list
+
     
     dice_list = []
     roi_mask_list = []
+    overlay_box_list = []
     # Iterate over the ROI list and crop the image
     for indexing, (x, y, w, h) in enumerate((roi_list)):
         try:
@@ -103,15 +167,39 @@ def perform_segmentation(predictor, image_path, box_list, expanded_rate=0.15, di
                     expanded = int(w*0.025)  
     
             #GT polygon to mask
+              
+                
             if segmentation_poly!="":
                 polygon = segmentation_poly[indexing]
-                binary_mask = polygon_to_mask(polygon, H, W)
+                if isinstance(polygon, list):
+#                     print ('list poly')
+                    binary_mask = polygon_to_mask(polygon, H, W)
+                    
+                elif type(polygon) == str:
+#                     print ('str poly')
+                    binary_mask = str_poly_to_mask(polygon, H, W)
+
+                else:
+                    print('print error nothing')
+            else:
+                print('print error segmentation_poly')
+            
+#             print (binary_mask)
     
             # Crop the image using numpy slicing
             cropped_image = image_source[int(y)-expanded:int(y+h)+expanded, int(x)-expanded:int(x+w)+expanded]
             
+#             print (binary_mask.shape)
+#             print (image_source.shape)
+            
+#             plt.imshow(binary_mask)
+#             plt.show()
             if segmentation_poly!="":
                 cropped_gt = binary_mask[int(y)-expanded:int(y+h)+expanded, int(x)-expanded:int(x+w)+expanded]
+#                 if isinstance(polygon, list):
+#                     cropped_gt = binary_mask[int(y)-expanded:int(y+h)+expanded, int(x)-expanded:int(x+w)+expanded]
+#                 elif type(polygon) == str:
+#                     cropped_gt = binary_mask[int(x)-expanded:int(x+w)+expanded, int(y)-expanded:int(y+h)+expanded]
     
             try:
                 sam_predictor.set_image(cropped_image)
@@ -122,7 +210,11 @@ def perform_segmentation(predictor, image_path, box_list, expanded_rate=0.15, di
                 
                 if segmentation_poly!="":
                     cropped_gt = binary_mask[int(y)-expanded:int(y+h)+expanded, int(x)-expanded:int(x+w)+expanded]
-    
+
+#             plt.imshow(cropped_image)
+#             plt.show()
+#             plt.imshow(cropped_gt)
+#             plt.show()
             bbox = [expanded, expanded, w+expanded, h+expanded]
     
             input_point = np.array([[int(w/2)+expanded, int(h/2)+expanded]])
@@ -153,7 +245,12 @@ def perform_segmentation(predictor, image_path, box_list, expanded_rate=0.15, di
                     cropped_gt = cropped_gt.squeeze(axis=-1)
                     dice = dice_coefficient(roi_mask, cropped_gt)
                 except:
-                    dice = 0
+                    try:
+                        dice = dice_coefficient(roi_mask, cropped_gt)
+                    except:
+                        #make an error
+                        dice = dice_coefficient(roi_mask, cropped_gt)
+                        dice = 0
                 dice_list.append(dice)
     
             if visualization:
@@ -166,20 +263,30 @@ def perform_segmentation(predictor, image_path, box_list, expanded_rate=0.15, di
     
                 random_color = np.random.randint(0, 255, (3,), dtype=np.uint8)
                 alpha = 0.7
-    
+                
+                
     
                 for i in range(over_h-1):
                     for j in range(over_w-1):
                         try:
                             if roi_mask[i, j]:
+                                #main image
                                 existing_pixel = main_image[over_y + i, over_x + j].astype(float)
                                 blended_pixel = (1 - alpha) * existing_pixel + alpha * random_color
                                 main_image[over_y + i, over_x + j] = np.clip(blended_pixel, 0, 255).astype(np.uint8)
+                                
+                                #overlab coop
+                                existing_pixel = cropped_image[i, j].astype(float)
+                                blended_pixel = (1 - alpha) * existing_pixel + alpha * random_color
+                                cropped_image[i, j] = np.clip(blended_pixel, 0, 255).astype(np.uint8)
+                                
                         except:
                             continue
-    
-    
-    
+                
+#                 plt.imshow(cropped_image)
+#                 plt.show()
+                overlay_box_list.append(cropped_image)
+                
                 #GT mask visualization
                 if segmentation_poly!="":
                     for i in range(over_h):
@@ -201,7 +308,7 @@ def perform_segmentation(predictor, image_path, box_list, expanded_rate=0.15, di
                 # Add text to the bounding box
                 if show_text:
                     text = label_list[indexing] +":"+ str(np.around(dice, decimals=2))
-                    font_scale = 0.3
+                    font_scale = 1#0.3
                     font_color = (255, 255, 255)  # White color for the text
                     font_thickness = 1
                     font = cv2.FONT_HERSHEY_SIMPLEX
@@ -236,7 +343,7 @@ def perform_segmentation(predictor, image_path, box_list, expanded_rate=0.15, di
 #         plt.imshow(gt_image)
 #         plt.title("GT")
 
-    return image_source, main_image, gt_image, dice_list, box_list, roi_mask_list
+    return image_source, main_image, gt_image, dice_list, box_list, roi_mask_list, overlay_box_list
 
 # Process multiple images and annotations from a JSON file
 def SAM_segmentatio_json(sam_predictor, data_list, dataset_path):
@@ -307,7 +414,7 @@ def open_image_with_nothing(file_path):
                 return local_image_path
             
 
-            
+#working on this ne
 def SAM_segmentatio_json_detect(sam_predictor, data_list, dataset_path):
     # Load JSON data
 #     with open(json_path, 'r') as f:
@@ -322,21 +429,42 @@ def SAM_segmentatio_json_detect(sam_predictor, data_list, dataset_path):
     local_image_paths = []
     roi_mask_lists = []
     label_list = []
-    for data in data_list:
-        image_path = data['image'][0].replace("@","/")
-        box_list = []
-        object_annotations = data['object_annotations']
+    data = data_list
+# for data in data_list:
+    image_path = data['image'][0].replace("@","/")
+    image_size = data['size']
+    box_list = []
+    segmentation_poly = []
+    object_annotations = data['object_annotations']
+    if object_annotations != []:
         for object_annotation in object_annotations:
-            box_list.append(object_annotation['bbox'])
             label_list.append(object_annotation['label'])
+            segmentation_poly.append(object_annotation['segmentation'])
+
+            
+            if object_annotation['bbox'] == "":
+                box_list.append(multipolygon_to_box(poly_sting_to_multi_polygon(object_annotation['segmentation'])))
+            else:
+                box_list.append(object_annotation['bbox'])
+            
+            
+            
+            
+#             for seg_obj, box_obj in zip(segmentation_poly, box_list):
+#                 if box_obj =="":
+#                     roi_list.append(multipolygon_to_box(poly_sting_to_multi_polygon(seg_obj)))
+#                 else:
+#                     roi_list.append(box_obj)
+            
+
         #label_list = [data['score']]
-        
+
         local_image_path =  open_image_with_nothing(image_path)
-                
-        
+
+
 
         local_image_paths.append(local_image_path)
-        
+
         xywh_boxes = []
         if box_list[0] != []:
             for box in box_list:
@@ -349,8 +477,8 @@ def SAM_segmentatio_json_detect(sam_predictor, data_list, dataset_path):
 
             #print ('xywh_boxes',xywh_boxes)
             # Perform segmentation
-            
-            image_source, main_image, gt_image, dice_list, box_list, roi_mask_list = perform_segmentation(sam_predictor, local_image_path, xywh_boxes, dice_cal=False, segmentation_poly="", label_list=label_list)
+
+            image_source, main_image, gt_image, dice_list, box_list, roi_mask_list, overlay_box_list = perform_segmentation(sam_predictor, local_image_path, xywh_boxes, dice_cal=True, segmentation_poly=segmentation_poly, label_list=label_list, show_text=True)
 
             image_sources.append(image_source)
             main_images.append(main_image)
@@ -363,6 +491,71 @@ def SAM_segmentatio_json_detect(sam_predictor, data_list, dataset_path):
             image_source = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             image_sources.append(image_source)
             main_images.append(image_source)
+        
+    return image_sources, main_images, gt_images, dice_lists, box_lists, roi_mask_lists, local_image_paths, label_list, overlay_box_list
+
+#hbb_detection_toyset
+def SAM_segmentatio_json_detect_Backup(sam_predictor, data_list, dataset_path):
+    # Load JSON data
+#     with open(json_path, 'r') as f:
+#         json_data = json.load(f)
+
+    data_list = data_list#json_data['data']
+    image_sources = []
+    main_images = []
+    gt_images = []
+    dice_lists = []
+    box_lists = []
+    local_image_paths = []
+    roi_mask_lists = []
+    label_list = []
+    for data in data_list:
+        print ("DATA:", data)
+        image_path = data['image'][0].replace("@","/")
+        try:
+            crop=data['crop']
+        except:
+            crop =[]
+        box_list = []
+        object_annotations = data['object_annotations']
+        if object_annotations != []:
+            for object_annotation in object_annotations:
+                box_list.append(object_annotation['bbox'])
+                label_list.append(object_annotation['label'])
+            #label_list = [data['score']]
+
+            local_image_path =  open_image_with_nothing(image_path)
+
+
+
+            local_image_paths.append(local_image_path)
+
+            xywh_boxes = []
+            if box_list[0] != []:
+                for box in box_list:
+                    x1, y1, x2, y2 =  box  # Convert strings to integers
+                    x = x1
+                    y = y1
+                    w = x2 - x1
+                    h = y2 - y1
+                    xywh_boxes.append([x, y, w, h])
+
+                #print ('xywh_boxes',xywh_boxes)
+                # Perform segmentation
+
+                image_source, main_image, gt_image, dice_list, box_list, roi_mask_list = perform_segmentation(sam_predictor, local_image_path, xywh_boxes, dice_cal=False, segmentation_poly="", label_list=label_list,crop=crop)
+
+                image_sources.append(image_source)
+                main_images.append(main_image)
+                gt_images.append(gt_image)
+                dice_lists.append(dice_list)
+                box_lists.append(box_list)
+                roi_mask_lists.append(roi_mask_list)
+            else:
+                image = cv2.imread(local_image_path)
+                image_source = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                image_sources.append(image_source)
+                main_images.append(image_source)
         
     return image_sources, main_images, gt_images, dice_lists, box_lists, roi_mask_lists, local_image_paths
 
