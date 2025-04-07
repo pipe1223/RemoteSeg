820a708efc62ee8e2911eab1803b7b3733532e01
Pipe <titinunt121@hotmail.com>
2025-04-07 15:08:39 +0800

    update post processing

--- post_process_sam_box.py
+++ post_process_sam_box.py
@@ -18,8 +18,16 @@ from shapely.geometry import Polygon, MultiPolygon
 import post_process
 #data clearning
 
+# Calculates the Dice coefficient between two masks (a measure of similarity)
+def dice_coefficient(mask1, mask2):
+    mask1 = mask1 > 0  # Convert to binary
+    mask2 = mask2 > 0
+    intersection = np.logical_and(mask1, mask2).sum()
+    union = np.logical_or(mask1, mask2).sum()
+    dice = 2. * intersection / (intersection + union)  # Avoid division by zero
+    return dice
 
-def perfrom_postprocess(df_path = "/datadisk/xyy/codes/Evaluation/post_output.csv"):
+def perfrom_postprocess(df_path = "/datadisk/pipe/results/sam_box/20250402114250408401/toy_set/Annotations_val/output.csv"):
     df = pd.read_csv(df_path)#'/home/datadisk/pipe/results/sam_box/output.csv')
     
     df['area'] = df['roi'].apply(
@@ -28,18 +36,40 @@ def perfrom_postprocess(df_path = "/datadisk/xyy/codes/Evaluation/post_output.cs
             and ((coords[2] - coords[0]) * (coords[3] - coords[1]))  # Compute area
     ))
 
+    dataset_name = 'toyset'
+    file_testing_name = 'post'
+    csv_path = "/home/datadisk/pipe/results/sam_box/post_"+datetime.now().strftime('%Y%m%d%H%M%S%f')+"/"+dataset_name+"/"
+    post_box_dir = csv_path+file_testing_name+"/"
+    if not os.path.exists(post_box_dir):
+        # Create the directory
+        os.makedirs(post_box_dir)
+        
     df['post_dice'] = np.nan
     df['post_path'] = np.nan
     
     #index = 5
     for index, row in df.iterrows():
-        
-        mask = cv2.imread(row['path'].replace("toy_set","toy_set2"))
+        gtp = row['gt_path'].replace('/home','')
+        print (gtp)
+        gt_mask = cv2.imread(gtp)
+        mask = cv2.imread(row['path'])
         post_mask = post_process.post_process(mask,row['area'],row['class'])
+
+        post_final_path = post_box_dir+datetime.now().strftime('%Y%m%d%H%M%S%f')+".jpg"
+        
+
         
         # calculate dice
         #save post_mask result
-        row['post_dice'] = 0
-        row['post_path'] = 'aaaa'
 
+        df.iloc[index, df.columns.get_loc('post_dice')] = dice_coefficient(gt_mask, post_mask)
+        df.iloc[index, df.columns.get_loc('post_path')] = post_final_path
+
+
+        cv2.imwrite(post_final_path, post_mask)
+
+    df.to_csv(csv_path+'output.csv', index=False)
     #save df
+
+
+perfrom_postprocess()
